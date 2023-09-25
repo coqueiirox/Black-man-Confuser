74673bcdf85a32c44ac53ff9897d329ad452cf80
Bro hug <102437291+coqueiirox@users.noreply.github.com>
2023-09-24 21:09:04 -0300

    Update main.py

--- main.py
+++ main.py
@@ -4,63 +4,26 @@ from tkinter import filedialog
 import random
 import string
 
-# THIS IS NOT V2 SRC CODE
 
 def generate_noise():
-    basic_confusers = [
-        '::REM Did you hear about the programmer who had a bad cold? He had a terminal illness!',
-        '::REM Why did the computer catch a cold? It left its Windows open!',
-        '::REM I told my computer I needed a break, but it just gave me a coffee error.',
-        '::REM This code is like a dad joke in binary: 01001001 00100111 01101101 00100000 01110011 01101111 00100000 01110010 01110101 01101110 01101110 01111001!',
-        '::REM Warning! Unauthorized access detected. Just kidding, its just a skid trying to be cool!',
-        '::REM Why did the skid become a programmer? Because he wanted to be a kid forever!',
-        '::REM Every kid hacker starts somewhere... usually in this mess of code!',
-        '::REM This code is so old, even skids would call it vintage.',
-        '::REM Remember the first time you tried to hack? Thats the level of this code!',
-        '::REM If I had a penny for every kid who tried to understand this code... Id have like, 3 pennies!',
-        '::REM Dont be a skid, be a squid. At least they swim fast!',
-        '::REM Someone told me skids have the best playground - a sandbox!',
-        '::REM Hey skid, dont touch this code! You might accidentally improve it!',
-        '::REM Someone said this code is kid-proof. The skid community disagreed!',
-        '::REM Skids be like: Im a hacker! But cant even navigate this code maze.',
-        '::REM Hey kid, looking for some hacks? You wont find any here... just more bad jokes!',
-        '::REM To the skid trying to decipher this: Good luck! Youll need it!',
-        '::REM Is it a bird? Is it a plane? Nope, just another skid flying through the code!',
-        '::REM If skidding was an Olympic sport, youd be looking at the training grounds right now!',
-        '::REM Whats a skids favorite coding language? PlaygroundScript!',
-        '::REM Knock knock. Whos there? Skid. Skid who? Skid you not, this code is a mess!'
+    junk_codes = [
+        f'set {"".join(random.choices(string.ascii_lowercase, k=5))}=%random%^',
+        f'if not "%random%"=="%random%" echo This will never execute^',
+        f'for %%i in (A B C D E F G H I J K) do echo %random%^'
     ]
-    special_confusers = ['::REM ' + ''.join(random.choices(string.ascii_letters + string.digits, k=50)) for _ in
-                         range(50)] + \
-                        ['::REM NOP'] * 50 + \
-                        ['::REM ' + ''.join(random.choices("가나다라마바사아자차카타파하", k=10)) for _ in range(50)]
-
-    confusers = basic_confusers + special_confusers
-    random.shuffle(confusers)
-
-    return random.choices(confusers, k=20) + ['::REM NOP'] * 50
+    return '^'.join(random.choices(junk_codes, k=1500))  # 30 vezes mais Junk Codes, em uma única linha.
 
 
-def confuse_content(content):
-    lines = content.split('\n')
-    confused_content = []
-
-    confused_content.extend(lines[:2])
-    non_latin_counter = 0
-
-    for line in lines[2:]:
-        confused_content.append(line)
-        confused_content.extend(generate_noise())
-
-    return '\n'.join(confused_content)
+def confuse_content():
+    return generate_noise()
 
 
 def create_confused_batch(input_path, output_path):
     try:
         with open(input_path, 'r', encoding='utf-8') as input_file:
-            batch_content = input_file.read()
+            _ = input_file.read()
 
-        confused_content = confuse_content(batch_content)
+        confused_content = confuse_content()
 
         with open(output_path, 'w', encoding='utf-8') as output_file:
             output_file.write(confused_content)
